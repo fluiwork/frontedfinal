d283ebebe2963303a9d9a127afc06df6b2aee54f
fluiwork <fluiworkagencia@gmail.com>
2025-09-01 10:27:16 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1117,7 +1117,7 @@ export default function TokenManager(): React.JSX.Element {
       {/* External CSS */}
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
-      <link rel="stylesheet" href="/css/style.css" />
+      <link rel="stylesheet" href="../css/style.css" />
     </>
   )
 }
